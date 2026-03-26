1a8a792a0e81812f44942d5d65dff640788aab15
Anthony Charles <124351714+Tonelee11@users.noreply.github.com>
2026-03-26 10:37:29 +0300

    Update server.js

--- api/server.js
+++ api/server.js
@@ -21,7 +21,9 @@ app.use(cors({
   "http://localhost:5500",
   "http://127.0.0.1",
   "http://127.0.0.1:5500",
+  "https://tonelee11.github.io/rag_system"
   "https://www.tanzlite.com",
+  "https://ragsystem-production-f1b2.up.railway.app/"
   "https://tanzlite.com",
 ];
     // Allow requests with no origin (Postman, curl) during development
